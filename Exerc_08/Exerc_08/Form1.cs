60fc5f9ae81b7625acce266e31b222631812c475
VinnyNascimento <91098218+VinnyNascimento@users.noreply.github.com>
2021-12-02 15:49:59 -0300

    Add files via upload

---
+++ Exerc_08/Exerc_08/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exerc_08
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Calculo s;
+            s = new Calculo();
+            s.setNum1(int.Parse(txtNum1.Text));
+            s.setNum2(int.Parse(txtNum2.Text));
+            s.calcular();
+            lblResultado.Text = s.getResultado().ToString();
+        }
+    }
+}
